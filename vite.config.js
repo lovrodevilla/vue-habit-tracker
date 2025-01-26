69499259cb739f8022152e5ad697a4be528ac19e
lovrodevilla <ld54153@fer.hr>
2025-01-26 20:47:03 +0100

    allow render domain

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     port: process.env.PORT || 3000, // Fallback to 3000 if no environment variable is set
     host: '0.0.0.0', // Required by Render to bind to the right network interface
+    allowedHosts: ['vue-habit-tracker.onrender.com'], // Allow the Render domain
   },
   plugins: [vue()],
 })
